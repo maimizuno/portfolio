99febe25b3f6337a29f564e8689fc1da6c984d05
Mai M <m.pooh0917@gmail.com>
2021-08-24 23:03:47 +1000

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,22 @@
+var startPos = 0, winScrollTop = 0;
+// scrollイベントを設定
+window.addEventListener('scroll', function () {
+    winScrollTop = this.scrollY;
+    if (winScrollTop >= startPos) {
+        // 下にスクロールされた時
+        if (winScrollTop >= 200) {
+            // 下に200pxスクロールされたら隠す
+            document.getElementById('scrollArea').classList.add('hide');
+        }
+    } else {
+        // 上にスクロールされた時
+        document.getElementById('scrollArea').classList.remove('hide');
+    }
+    startPos = winScrollTop;
+});
+
+
+
+//ロードとリサイズの両方で同じ処理を付与する
+window.onload = switchByWidth;
+window.onresize = switchByWidth;
